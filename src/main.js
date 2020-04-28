130224b4bb2060858853d41a7cf07858ea97cb45
magysil <62733263+magysil@users.noreply.github.com>
2020-04-28 19:32:27 -0400

    corrección cardTemplate de  tarjeta y Id enlace

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
 import Pokemones from "./data/pokemon/pokemon.js";
-import { allPokemon, orderPokemonAz, orderPokemonZa } from "./data.js";
+import { allPokemon, orderPokemonAz, orderPokemonZa,  } from "./data.js";
 
-const card = document.getElementById("contenidoTodos");
+const card = document.getElementById("viewCardCreate");
 
 //Arreglo con todos los datos de Pokemones
 const allData = allPokemon(Pokemones.pokemon);
 const orderedDataAz = orderPokemonAz(allData);
 const orderedDataZa = orderPokemonZa(allData);
+//const orderedDataDescendant = orderPokemonDescendant(allData);
 
 //Muestra todos lo Pokemones en pantalla
 function viewAllPokemon(dataSet) {
   let cardTemplate = "";
   for (let i = 0; i < dataSet.length; i++) {
     cardTemplate += `
-                             <div class="elementos">
+                             <div class="elementCard">
                              <div id='pokemon${dataSet[i].id}' class="card${dataSet[i].type[0]}">
                                 <div class='itemsPoke'> 
                                     <h2>${dataSet[i].name}</h2>
                                     <h3>${dataSet[i].num}</h3>
                                     <img class="imgPokemon" src="${dataSet[i].img}"/>
                                     <h3>${dataSet[i].type[0]} </h3>
-                                    </br> 
-                                    </br> 
                                 </div>
                             </div>
                             </div>`;
@@ -34,14 +33,19 @@ viewAllPokemon(allData);
 const selectElement = document.querySelector(".ordenPoke");
 selectElement.addEventListener("change", (e) => {
   const resultado = `${e.target.value}`;
-
+  //console.log(resultado);
   if (resultado === "A-Z") {
     viewAllPokemon(orderedDataAz);
+    
   }
   if (resultado === "Z-A") {
     viewAllPokemon(orderedDataZa);
-    
-  } else if (resultado === "Todos") {
+
+ /*  if (resultado === "151-1")  
+    viewAllPokemon(orderedDataDescendant); */
+
+  } else if (resultado === "All") {
     viewAllPokemon(allData);
   }
 });
+
