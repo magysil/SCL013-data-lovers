79b69a40c9cf057463474fc45cbe0ec07503d32f
magysil <62733263+magysil@users.noreply.github.com>
2020-04-30 23:11:12 -0400

    Test unitarios historia de usuario1 completado

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,97 @@
-import { example, anotherExample } from '../src/data.js';
+import { allPokemon, orderPokemonAz, orderPokemonZa } from "../src/data.js";
 
+const arrayAll = [
+  {
+    name: "Harry Potter",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+  },
 
-describe('example', () => {
-  test('is a function', () => {
-    expect(typeof example).toBe('function');
+  {
+    name: "Hermione Granger",
+    species: "human",
+    gender: "female",
+    house: "Gryffindor",
+  },
+
+  {
+    name: "Ron Weasley",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+  },
+];
+
+const arrayViewAll = [
+  {
+    name: "Harry Potter",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+  },
+
+  {
+    name: "Hermione Granger",
+    species: "human",
+    gender: "female",
+    house: "Gryffindor",
+  },
+  {
+    name: "Ron Weasley",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+  },
+];
+
+const arrayOriginal = [
+  { name: "Harry Potter" },
+  { name: "Draco Malfoy" },
+  { name: "Ron Weasley" },
+  { name: "Draco Malfoy" },
+];
+
+const arrayAz = [
+  { name: "Draco Malfoy" },
+  { name: "Draco Malfoy" },
+  { name: "Harry Potter" },
+  { name: "Ron Weasley" },
+];
+
+const arrayZa = [
+  { name: "Ron Weasley" },
+  { name: "Harry Potter" },
+  { name: "Draco Malfoy" },
+  { name: "Draco Malfoy" },
+];
+
+describe("allPokemon", () => {
+  test("is a function", () => {
+    expect(typeof allPokemon).toBe("function");
   });
 
-  test('returns `example`', () => {
-    expect(example()).toBe('example');
+  test("Mostrar todos los Datos`", () => {
+    expect(allPokemon(arrayAll)).toEqual(arrayViewAll);
   });
 });
 
-
-describe('anotherExample', () => {
-  test('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe("orderPokemonAz", () => {
+  test("is a function", () => {
+    expect(typeof orderPokemonAz).toBe("function");
   });
 
-  test('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  test("Oredenar los nombres de la A-Z", () => {
+    expect(orderPokemonAz(arrayOriginal)).toStrictEqual(arrayAz);
+  });
+});
+
+describe("orderPokemonZa", () => {
+  test("is a function", () => {
+    expect(typeof orderPokemonZa).toBe("function");
+  });
+
+  test("Ordenar los nombres de la Z-A", () => {
+    expect(orderPokemonZa(arrayOriginal)).toStrictEqual(arrayZa);
   });
 });
