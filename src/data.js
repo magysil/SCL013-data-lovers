1ee6b2647563c1f55a2de5bc906fab19257a6fa9
magysil <62733263+magysil@users.noreply.github.com>
2020-05-06 01:07:21 -0400

    b

--- src/data.js
+++ src/data.js
@@ -1,9 +1,65 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+//Trae todos lo registros de la data
+export const allPokemon = (dataSet) => {
+  const newDataArray = [];
+  for (let i = 0; i < dataSet.length; i += 1) {
+    newDataArray.push(dataSet[i]);
+  }
+  return newDataArray;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//Ordena Pokemones por nombre de la A a la Z
+export const orderPokemonAz = (dataSet)=>{
+  const newDataArray = [];
+  for (let i = 0; i < dataSet.length; i++) {
+    newDataArray.push(dataSet[i]);
+  }
+  newDataArray.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+  return newDataArray;
 };
+
+//Ordena Pokemones por nombre de la Z a la A
+export const orderPokemonZa = (dataSet)=>{
+  const newDataArray = [];
+  for (let i = 0; i < dataSet.length; i ++) {
+    newDataArray.push(dataSet[i]);
+  }
+  newDataArray.sort((a, b) => {
+    if (a.name > b.name) {
+      return -1;
+    }
+    if (a.name < b.name) {
+      return 1;
+    }
+    return 0;
+  });
+  return newDataArray;
+};
+
+
+//Ordena Pokemones por de manera descendiente 151 al 1
+/* export const orderPokemonDescendant = (dataSet)=>{
+  const newDataArray = [];
+  for (let i = 0; i < dataSet.length; i ++) {
+    newDataArray.push(dataSet[i]);
+  }
+  newDataArray.sort((a, b) => a.num-b.num {
+     if (a.num > b.num) {
+      return - 1;
+     } 
+     if (a.name < b.name) {
+      return 1;
+    }
+    return 0;
+     
+   })
+  return newDataArray;
+
+}; */
--- src/main.js
+++ src/main.js
@@ -1,6 +1,52 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import Pokemones from "./data/pokemon/pokemon.js";
+import { allPokemon, orderPokemonAz, orderPokemonZa  } from "./data.js";
 
-console.log(example, data);
+const card = document.getElementById("viewCardCreate");
+
+//Arreglo con todos los datos de Pokemones
+const allData = allPokemon(Pokemones.pokemon);
+const orderedDataAz = orderPokemonAz(allData);
+const orderedDataZa = orderPokemonZa(allData);
+//const orderedDataDescendant = orderPokemonDescendant(allData);
+
+//Muestra todos lo Pokemones en pantalla
+function viewAllPokemon(dataSet) {
+  let cardTemplate = "";
+  for (let i = 0; i < dataSet.length; i++) {
+    cardTemplate += `
+                             <div class="elementCard">
+                             <div id='pokemon${dataSet[i].id}' class="card${dataSet[i].type[0]}">
+                                <div class='itemsPoke'> 
+                                    <h2>${dataSet[i].name}</h2>
+                                    <h3>${dataSet[i].num}</h3>
+                                    <img class="imgPokemon" src="${dataSet[i].img}"/>
+                                    <h3>${dataSet[i].type.join(" - ")} </h3>
+                                </div>
+                            </div>
+                            </div>`;
+  }
+  card.innerHTML = cardTemplate;  
+}
+
+viewAllPokemon(allData);
+//console.log( allData);
+//Ordenar pokemones A-Z / Z-A / Regresar a todos
+const selectElement = document.querySelector(".ordenPoke");
+selectElement.addEventListener("change", (e) => {
+  const resultado = `${e.target.value}`;
+  //console.log(resultado);
+  if (resultado === "A-Z") {
+    viewAllPokemon(orderedDataAz);
+    
+  }
+  if (resultado === "Z-A") {
+    viewAllPokemon(orderedDataZa);
+
+ /*  if (resultado === "151-1")  
+    viewAllPokemon(orderedDataDescendant); */
+
+  } else if (resultado === "All") {
+    viewAllPokemon(allData);
+  }
+});
+
